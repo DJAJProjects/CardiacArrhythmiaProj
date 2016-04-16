6123feb5c73c26750a01caca24db0d3779c5ab16
kubawieczorek <jwieczorek94@wp.pl>
2016-04-16 17:42:27 +0200

    Bazowy projekt, biblioteka  ENCOG
    Bazowy projekt, biblioteka  ENCOG

---
+++ ArrhythmiaNN/src/main/java/Controller/Main.java
@@ -0,0 +1,12 @@
+package Controller;
+
+/**
+ * Created by Kuba on 16.04.2016.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
---
+++ ArrhythmiaNN/src/main/java/View/MainForm.java
@@ -0,0 +1,7 @@
+package View;
+
+/**
+ * Created by Kuba on 16.04.2016.
+ */
+public class MainForm {
+}
